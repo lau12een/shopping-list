1884d4fe5cb99a54bd551c3b4727d005cdd01505
lau12een <47163966+lau12een@users.noreply.github.com>
2019-03-02 22:17:29 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// To build an app like this we must start with triggers first
+// then you can figure out the functions
+
 $(function(){
-  
+
+  /******************* EVENT LISTENERS***********/
+// add - form submission button
+//form trigger
   $('#js-shopping-list-form').submit(function(event) {
     
     event.preventDefault();
@@ -22,17 +28,18 @@ $(function(){
         </li>`);
     });
     
+    //delete trigger - on-click ( no form)
      $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
   
       // this will look for the closest parent `li` element and remove it from the DOM.
-      // in other words, it will remove the shopping list item.
+      // Simply, it will delete items from the list
       $(this).closest('li').remove();
     });
-    
+    //check trigger - on click ( no form)
       $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
   
-      // go up to the parent li, then from there find the child`.shopping-item`  and
-      // toggle the `.shopping-item__checked` class.
+      // Parent li --> finds the child`.shopping-item`  and
+      // toggle the `.shopping-item__checked` class. (on = checked, off = unchecked)
       $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
     
